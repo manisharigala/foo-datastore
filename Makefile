32663cd80837fcfb1981c0d869a06626e58028dc
manisharigala <arigalamanish@gmail.com>
2022-03-29 00:18:45 -0700

    Added makefile

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	go build -o  bin/main  cmd/main/main.go
+
+run:
+	go run cmd/main/main.go
+
+clean:
+	rm -rf $(CURDIR)/bin
--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/manisharigala/Foo-Datastore/api"
@@ -8,5 +9,10 @@ import (
 
 func main() {
 	srv := api.NewServer()
-	http.ListenAndServe(":8080", srv)
+	err := http.ListenAndServe(":8080", srv)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Listening on Port 8080...")
 }
+ 
